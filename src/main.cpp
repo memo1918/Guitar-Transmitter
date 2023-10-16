bc0ae260ae3ffed3fdcb48cf6dd36d9897dba67c
Kai Mayer <kmayer1@stud.hs-heilbronn.de>
2023-10-16 22:14:52 +0200

    feat: add cmake config

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,16 @@
 int main()
 {
   const uint LED_PIN = 17;
+  stdio_init_all();
+
   gpio_init(LED_PIN);
   gpio_set_dir(LED_PIN, GPIO_OUT);
   while (true)
   {
     gpio_put(LED_PIN, 1);
-    sleep_ms(250);
+    sleep_ms(500);
     gpio_put(LED_PIN, 0);
-    sleep_ms(250);
+    sleep_ms(500);
+    printf("Hello World");
   }
 }
