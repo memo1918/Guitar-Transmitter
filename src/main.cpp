52c196324b1020b9f3ce829500f0181ba5ef0b0b
Kai Mayer <kmayer1@stud.hs-heilbronn.de>
2023-10-16 22:41:54 +0200

    feat: add adc sample

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
 
 int main()
 {
-  const uint LED_PIN = 17;
   stdio_init_all();
+  printf("ADC - GPIO26");
+  adc_init();
 
-  gpio_init(LED_PIN);
-  gpio_set_dir(LED_PIN, GPIO_OUT);
+  adc_gpio_init(26);
+  adc_select_input(0);
+
   while (true)
   {
-    gpio_put(LED_PIN, 1);
+    const float conversion_factor = 3.3f / (1 << 12);
+    uint16_t result = adc_read();
+    printf("Raw value: 0x%03x, voltage: %f V\n", result, result * conversion_factor);
     sleep_ms(500);
-    gpio_put(LED_PIN, 0);
-    sleep_ms(500);
-    printf("Hello World");
   }
 }
